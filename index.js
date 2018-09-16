a223d96a7d72c15b18984d7ff0438c1e83ef9a1b
Paul Clark <clark.paul123@gmail.com>
2018-09-16 19:43:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine, name) {
-  let currentLine = [];
-  for (i = 0; i < currentLine.length; i++)
-  currentLine.push()
   if (katzDeliLine.length === 0) {
   return "The line is currently empty.";
   } else {
+    let currentLine = [];
+  for (i = 0; i < currentLine.length; i++)
+  currentLine.push()
   return `The line is currently: ${katzDeliLine}.`;
   }
 }
